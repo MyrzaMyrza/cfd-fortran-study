a8b3f093c684f6d03ccd7ad58726b279f54c8f56
Myrzakhan Omarbay <m.omarbayev@gmail.com>
2026-09-24 20:28:06 +0500

    Разбор ошибок в учебной FTCS-проге-первый коммит

--- 01-ftcs-heat-conduction/2026.09.01_1d_thermalconductivity.f90
+++ 01-ftcs-heat-conduction/2026.09.01_1d_thermalconductivity.f90
@@ -1,75 +1,46 @@
-program 1d_thermal_conductivity
-  implicit none
-integer :: nx, nt   !целые цисла
-real :: alpha, dt   !вещественные числа
-real, parameter :: pi = 3.14159   !константа, менять нельзя
-
-real, allocate :: T(:)   !массив с неопределенным размером
-
-allocate(T(nx))   !память под nx элементов
-
 module heat_params
 	implicit none
 	real, parameter :: L = 1.0
 	integer, parameter :: nx = 21
 end module heat_params
 
-program main
-	use heat_params
-	implicit none
-end program main
+program thermal_conductivity_1d
+  use heat_params
+  implicit none
 
-T(1) = 100.0
-T(nx) = 0.0
+  integer :: nt
+  real :: alpha, dt, dx
+  real, parameter :: pi = 3.14159
 
-end program 1d_thermal_conductivity
+  real, allocatable :: T(:)
 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-  ! Initial condition: u(x,0) = sin(pi*x)
-  do i = 1, nx
-    u(i) = sin(pi * (i - 1) * dx)
-  end do
+  allocate(T(nx))
+  T = 0.0
+  T(1) = 100.0
+  T(nx) = 0.0
+  dx = L / (nx - 1)
+  alpha = 0.01
 
-  ! Time-stepping loop
-  do n = 1, nt
-    ! Update interior points using finite difference method
-    do i = 2, nx - 1
-      u_new(i) = u(i) + alpha * dt / dx**2 * (u(i + 1) - 2 * u(i) + u(i - 1))
-    end do
+  print *, "T(1) = ", T(1)
+  print *, "T(nx) = ", T(nx)
+  print *, "dx = ", dx
+  print *, "alpha = ", alpha
 
-    ! Apply boundary conditions (Dirichlet)
-    u_new(1) = u(1)
-    u_new(nx) = u(nx)
+end program thermal_conductivity_1d
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 
-    ! Update the solution for the next time step
-    u = u_new
-  end do
-
-  ! Output the final temperature distribution
-  print *, "Final temperature distribution:"
-  do i = 1, nx
-    print *, (i - 1) * dx, u(i)
-  end do
-
-end program 1d_thermal_conductivity
